1f260da5ceceb69eac892eb25420c85d5438d61b
Admin <admin@Addapps-MacBook-Pro.local>
2021-02-02 22:25:18 -0800

    WIP

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = ({ Component, pageProps }) => <Component {...pageProps} />;
+
+export default App;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "reactstrap";
+
+const Index = () => {
+  return (
+      <Container>
+          <h1>Welcome to Onyx Creative labs</h1>
+      </Container>
+  );
+};
+
+export default Index;
